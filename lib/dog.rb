1d480db62b3931100fc1d359e87e7994a04ff5c4
Alyssa Yates <bosslys@icloud.com>
2019-09-16 18:10:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,2 @@
 class Dog
-
-  def name=(dogs_name)
-    @this_dogs_name = dog_name
-  end
-
-  def name
-    @this_dogs_name
-  end
-end
-
-lassie = Dog.new
-lassie.name = "Lassie"
-
-puts lassnie.name
+end 
